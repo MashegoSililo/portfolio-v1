c12308e569ef8f6723ca02d4e871a5b687692017
Mashego Sililo <isthmussa@gmail.com>
2022-01-20 15:46:04 +0200

    Project read-more buttons and information modals

---
+++ app.js
@@ -0,0 +1,12 @@
+function openModal(id) {
+  var modal = document.getElementById(id);
+  modal.style.display = "block";
+  
+}
+
+function closeModal(id) {
+  var modal = document.getElementById(id);
+  modal.style.display = "none";
+}
+
+
